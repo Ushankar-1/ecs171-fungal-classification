688b4238ff4965fe6f79f3106fcc85da07afb79a
Hawkhobo <jacob.d.feenstra@gmail.com>
2023-11-28 23:21:07 -0800

    error in defungi.ipynb
    Line 31, tf.strings.regex_replace() returns a tensorflow string (it is simple the class, 'H1', 'H2', so on). Trying to convert this string to a simple string to index a dictionary. Having a very hard time with this tensorflow string -> string conversion for some reason.

--- Experiments/run_all_models.sh
+++ Experiments/run_all_models.sh
@@ -1,7 +1,7 @@
 # Run dataset on CDIL-CNN, and other ML models, for cross-analysis 
-python3 lra_main.py --model CDIL
-python3 lra_main.py --model TCN
-python3 lra_main.py --model CNN
-python3 lra_main.py --model Deformable
-python3 lra_main.py --model GRU
-python3 lra_main.py --model LSTM
+jupyter nbconvert --to notebook --execute main.ipynb --model CDIL
+jupyter nbconvert --to notebook --execute main.ipynb --model TCN
+jupyter nbconvert --to notebook --execute main.ipynb --model CNN
+jupyter nbconvert --to notebook --execute main.ipynb --model Deformable
+jupyter nbconvert --to notebook --execute main.ipynb --model GRU
+jupyter nbconvert --to notebook --execute main.ipynb --model LSTM
