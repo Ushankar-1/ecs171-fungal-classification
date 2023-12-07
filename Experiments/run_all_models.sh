eb32d9e22613fc8432575462a8e1b954db77e731
Hawkhobo <jacob.d.feenstra@gmail.com>
2023-12-07 10:01:39 -0800

    codespace cleaned

--- Experiments/run_all_models.sh
+++
@@ -1,7 +0,0 @@
-# Run dataset on CDIL-CNN, and other ML models, for cross-analysis 
-jupyter nbconvert --to notebook --execute main.ipynb --model CDIL
-jupyter nbconvert --to notebook --execute main.ipynb --model TCN
-jupyter nbconvert --to notebook --execute main.ipynb --model CNN
-jupyter nbconvert --to notebook --execute main.ipynb --model Deformable
-jupyter nbconvert --to notebook --execute main.ipynb --model GRU
-jupyter nbconvert --to notebook --execute main.ipynb --model LSTM
